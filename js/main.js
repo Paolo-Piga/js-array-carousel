22dc18b55c2b98a56aa6e1d61dbfbc29d0ea5449
DecimTheJudge <paolo1piga@gmail.com>
2024-06-10 17:22:29 +0200

    inizio

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const carousel = [
+    `img/01.webp`,
+    `img/02.webp`,
+    `img/03.webp`,
+    `img/04.webp`,
+    `img/05.webp`
+];
+
+function move (amount){
+    const imgCar = items.length;
+    return carousel[currentIndex];
+}
